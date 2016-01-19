e863c49a5f5765f75ea178b0f5f5845170ccbc7d
JANGHEEJEONG <01692204427@naver.com>
2016-01-19 17:52:46 +0900

    add bubble

---
+++ algorithm/bubble.java
@@ -0,0 +1,37 @@
+/**
+ * Created by janghuijeong on 2016. 1. 14..
+ */
+public class Test {
+
+    int arr[] = {1, 2, 9, 5, 7, 3};
+
+    public static void main(String[] arg) {
+        Test m = new Test();
+        m.bublle();
+    }
+
+    void bublle() {
+        int temp = 0;
+        System.out.println("Before");
+
+        for(int cnt=0; cnt<6; cnt++) {
+            System.out.print(arr[cnt]);
+        }
+
+        System.out.println();
+
+        for(int i=0; i<6; i++) {
+            for(int j=0;j<(6-i)-1; j++) {
+                if(arr[j]>arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("After");
+        for(int cnt=0; cnt<6; cnt++) {
+            System.out.print(arr[cnt]);
+        }
+    }
+}
