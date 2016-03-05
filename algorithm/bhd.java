3961bf00c3e288fde4302c938f9b2a810dc78052
JANGHEEJEONG <01692204427@naver.com>
2016-03-05 19:21:29 +0900

    bhd in doublet

---
+++ algorithm/bhd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] argc) {
+        long count = 0;
+        Scanner scanner = new Scanner(System.in);
+        int input = 0;
+        input = scanner.nextInt();
+        long arr[] = new long[input];
+        for(int i=0; i<input; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i=0; i<input; i++) {
+            for(int j=i+1; j<input; j++) {
+                if(arr[i] > arr[j]) {
+                    count++;
+                }
+                else
+                    break;
+            }
+        }
+        System.out.println(count);
+    }
+}
